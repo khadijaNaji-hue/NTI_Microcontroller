7af9457093e9e56ef7c767a1cae95d7bb8726644
khadijaNaji-hue <khadeja.mn005@gmail.com>
2026-07-29 11:19:24 +0300

    Update Bit_Math.h

--- Drivers/STD_LID/Bit_Math.h
+++ Drivers/STD_LID/Bit_Math.h
@@ -5,6 +5,7 @@
 #define CLR_BIT(REG,BIT_NUM)     REG &= ~ (1<<BIT_NUM)
 #define TOG_BIT(REG,BIT_NUM)     REG ^=   (1<<BIT_NUM)
 #define Get_Bit(REG,BIT_NUM)    (REG>>BIT_NUM)&(0X01)
+#define SET_BYTE(REG,VALUE)      REG  =   VALUE
 #define SET_BYTE(REG)            REG  =   (0xFF)
 #define CLR_BYTE(REG)            REG  =   (0x00)
 
