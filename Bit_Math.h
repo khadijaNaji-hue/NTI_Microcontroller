5ce1b59aad1e946770172945435616e9fac869ae
khadijaNaji-hue <khadeja.mn005@gmail.com>
2026-07-29 11:05:34 +0300

    Add files via upload

---
+++ Bit_Math.h
@@ -0,0 +1,10 @@
+#ifndef BIT_MATH_H_
+#define BIT_MATH_H_
+  
+#define SET_BIT(REG,BIT_NUM)     REG |=(1<<BIT_NUM)
+#define CLR_BIT(REG,BIT_NUM)     REG &= ~ (1<<BIT_NUM)
+#define TOG_BIT(REG,BIT_NUM)     REG ^=   (1<<BIT_NUM)
+#define Get_Bit(REG,BIT_NUM)    (REG>>BIT_NUM)&(0X01)
+#define  SET_BYTE(REG)           REG  =   (0xFF)
+#define  CLR_BYTE(REG)           REG  =   (0x00)
+#endif
