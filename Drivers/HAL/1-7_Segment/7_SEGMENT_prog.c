2ee0c96d9640f462fc174bfde8d2ce9169b31e87
khadijaNaji-hue <khadeja.mn005@gmail.com>
2026-07-29 17:30:22 +0300

    Update 7_SEGMENT_prog.c

--- Drivers/HAL/1-7_Segment/7_SEGMENT_prog.c
+++ Drivers/HAL/1-7_Segment/7_SEGMENT_prog.c
@@ -13,6 +13,6 @@ u8 _7SEGMENT_u8DecTo7segConvert (u8 decimalNo , u8 testLED_state)
 {
 	u8 noIn7SEG[10]= {126,48,109,121,51,91,95,112,127,123};
 	if ( testLED_state== ON )
-	{ SET_BIT((noIn7SEG[decimalNo] , 7);}
+		SET_BIT(noIn7SEG[decimalNo] , 7);
 	return (noIn7SEG[decimalNo]);
 }
