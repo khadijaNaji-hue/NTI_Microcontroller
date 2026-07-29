15f5236c17913912769ba5bd5700ce5566bf37bd
khadijaNaji-hue <khadeja.mn005@gmail.com>
2026-07-29 17:31:00 +0300

    Update 7_SEGMENT_config.h

--- Drivers/HAL/1-7_Segment/7_SEGMENT_config.h
+++ Drivers/HAL/1-7_Segment/7_SEGMENT_config.h
@@ -8,8 +8,8 @@
 #define 7_SEGMENT_CONFIG_H
 
 /** on off are alreadefined at DIO_int.h , Not sure if i should redefine here or not*/
-#define ON  1 
-#define OOF 0
+//#define ON  1 
+//#define OOF 0
 
 
 #endif
