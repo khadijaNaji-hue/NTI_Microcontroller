8d13fdf5e566344e0e043e9ede71d548733bb37f
khadijaNaji-hue <khadeja.mn005@gmail.com>
2026-07-31 16:56:46 +0300

    Add files via upload

---
+++ Drivers/MCAL/2-INT/INT.prog.c
@@ -0,0 +1,15 @@
+/************************************************/
+/************** Author: Khadija Naji ************/
+/************** Date  : 31/07/2026   ************/
+/************** File  : Program File  ***********/
+/********* Last Update: 31/07/2026   ************/
+/************************************************/
+
+#include "Bit_Math.h"
+#include "STD_Types.h"
+
+#include "DIO_int.h"
+
+#include "INT_int.h"
+#include "INT_config.h"
+#include "INT_private.h"
